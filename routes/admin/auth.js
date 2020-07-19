e975ccce7c64793c809cfa1177efd13b2e46eb7a
NightmanCZ90 <novak.jan90@gmail.com>
2020-07-19 19:58:12 +0200

    add template helper functions

--- routes/admin/auth.js
+++ routes/admin/auth.js
@@ -48,7 +48,7 @@ router.get('/signout', (req, res) => {
 });
 
 router.get('/signin', (req, res) => {
-  res.send(signinTemplate());
+  res.send(signinTemplate({}));
 });
 
 router.post(
@@ -59,7 +59,10 @@ router.post(
   ], 
   async (req, res) => {
     const errors = validationResult(req);
-    console.log(errors);
+
+    if (!errors.isEmpty()) {
+      return res.send(signinTemplate({ errors }));
+    }
 
     const { email } = req.body;
 
