2a873a749d4b882be63a7ed85236cd20eda52010
NightmanCZ90 <novak.jan90@gmail.com>
2020-07-20 02:59:18 +0200

    reusable error handling middleware

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -1,7 +1,7 @@
 const express = require('express');
-const { validationResult } = require('express-validator');
 const multer = require('multer');
 
+const { handleErrors } = require('./middlewares');
 const productsRepo = require('../../repositories/products');
 const productsNewTemplate = require('../../views/admin/products/new');
 const { requireTitle, requirePrice } = require('./validators');
@@ -24,13 +24,8 @@ router.post(
     requireTitle, 
     requirePrice
   ],
+  handleErrors(productsNewTemplate),
   async (req, res) => {
-    const errors = validationResult(req);
-
-    if (!errors.isEmpty()) {
-      return res.send(productsNewTemplate({ errors }));
-    }
-
     const image = req.file.buffer.toString('base64');
     const { title, price } = req.body;
     await productsRepo.create({ title, price, image });
