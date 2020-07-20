9804b31315144788c67512462b1599b7d55aed1e
NightmanCZ90 <novak.jan90@gmail.com>
2020-07-21 01:14:14 +0200

    added redirection on success actions

--- routes/admin/auth.js
+++ routes/admin/auth.js
@@ -35,7 +35,7 @@ router.post(
     // Store the id of that user inside the users cookie
     req.session.userId = user.id;
 
-    res.send('Account created');
+    res.redirect('/admin/products');
 });
 
 router.get('/signout', (req, res) => {
@@ -65,7 +65,7 @@ router.post(
 
     req.session.userId = user.id;
 
-    res.send('You are signed in')
+    res.redirect('/admin/products');
   }
 );
 
